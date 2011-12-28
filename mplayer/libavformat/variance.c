9c4164803bed4e4895fb891037fad8ed56b33095
Daniel <pikgen@sina.com.cn>
2011-12-28 14:24:58 +0800

    Improve double calculate precision to avoid some playing bugs (小鬼神偷（1）)

--- mplayer/libavformat/variance.c
+++ mplayer/libavformat/variance.c
@@ -1,16 +1,15 @@
 
-#define MAX_STD_TIMEBASES (60*12+5)
-
-extern float time_adj_data[];
 static double error __attribute__ ((aligned (8)));
 
-void variance_cal (double *res, double *dur)
+void variance_cal (double *res, double *dur, int cnt)
 {
-   int i;
-   for(i=1; i<MAX_STD_TIMEBASES; i++){
-     int ticks = (int)(*dur * time_adj_data[i] + 0.5);
-     error = *dur - ticks/time_adj_data[i];
-     res[i] += error * error;
-   }
+	int i; 
+  for(i=1; i<cnt; i++){
+    int framerate= get_std_framerate(i);
+    int ticks= (int)(*dur*framerate/(1001*12) + 0.5);
+    
+    error= *dur - ticks*1001*12/(double)framerate;
+    res[i] += error*error;
+  }
 }
 
